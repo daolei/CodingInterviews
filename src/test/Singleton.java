9fa31cb4d48e3ad6c783fcef565dca4d9140df00
daolei.su <daolei3935@gmail.com>
2015-11-22 20:46:26 +0800

    ehanshi

--- src/test/Singleton.java
+++ src/test/Singleton.java
@@ -1,5 +1,10 @@
 package test;
 
 public class Singleton {
+	private Singleton(){}
+	private static Singleton instance = new Singleton();
+	public static Singleton Instance(){
+		return instance;
+	}
 
 }
